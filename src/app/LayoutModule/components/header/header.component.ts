2dd466d94be9a9e879f1f7545a18634ac4865f94
Asmaa Hassan Hamza <asmaahassanhamza1@gmail.com>
2024-09-08 23:08:10 +0300

    fixes

--- src/app/LayoutModule/components/header/header.component.ts
+++ src/app/LayoutModule/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,11 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+
+  constructor(private router:Router){}
+
+  onNavigate(){
+    this.router.navigate(['/details?from=EUR&to=USD&amount=1'])
+  }
 
 }
